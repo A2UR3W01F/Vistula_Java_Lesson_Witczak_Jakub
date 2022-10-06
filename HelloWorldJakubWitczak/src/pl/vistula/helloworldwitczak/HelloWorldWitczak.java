41e2d15a12ee30bce2e8e43c5fa5ca26c18db695
Jakub Witczak <codename.a2ur3w01f@gmail.com>
2022-10-06 16:14:24 +0200

    Nauka javy - first commit

--- HelloWorldJakubWitczak/src/pl/vistula/helloworldwitczak/HelloWorldWitczak.java
+++ HelloWorldJakubWitczak/src/pl/vistula/helloworldwitczak/HelloWorldWitczak.java
@@ -4,6 +4,10 @@ public class HelloWorldWitczak {
     public static void main(String[] args) {
         System.out.println("Hello World Vistula");
 
-        System.out.println("Sprawdzamy modyfikacje");
+        System.out.println("Pauza 1");
+
+        System.out.println("Pauza 2");
+
+        System.out.println("Pauza 3");
     }
 }
